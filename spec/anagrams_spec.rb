306b551688f28eba7a9408e2b60541f9e36a4af1
Mike Wright <m.wright92@icloud.com>
2022-05-06 08:45:07 -0700

    write test for checking for case sensitivity, solved by initial setup of class

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -7,4 +7,8 @@ describe '#anagram?' do
     words = Anagrams.new("eat", "tea")
     expect(words.compare).to(eq(true))
   end
+  it('should return true if two inputted words are anagrams even if in different cases') do
+    words = Anagrams.new("eAt", "TeA")
+    expect(words.compare).to(eq(true))
+  end
 end
