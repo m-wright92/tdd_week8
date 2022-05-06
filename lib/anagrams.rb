b59f6d5c85459133e7160c7e9ddc8322810959f7
Mike Wright <m.wright92@icloud.com>
2022-05-06 08:42:04 -0700

    pass first rspec test for compare method

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,10 +1,16 @@
 class Anagrams
   def initialize(string1, string2)
-    @string1 = string1
-    @string2 = string2
+    @string1 = string1.downcase
+    @string2 = string2.downcase
   end
 
   def compare
-    false
+    string1_array = @string1.chars.sort
+    string2_array = @string2.chars.sort
+    if string1_array == string2_array
+      return true
+    else
+      return false
+    end
   end
 end
