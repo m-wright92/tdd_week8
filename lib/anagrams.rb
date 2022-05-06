dc817bb59829afe0d2c6f60aa1e05e814ac7ec7c
Mike Wright <m.wright92@icloud.com>
2022-05-06 10:16:54 -0700

    add test for response of non-anagrams, WIP test for antigram

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -10,9 +10,9 @@ class Anagrams
     string2_array = @string2.chars.sort
     if ((@string1.count "#{@vowels}") > 0) && ((@string2.count "#{@vowels}") > 0)
       if string1_array == string2_array
-        return true
+        return "Look, an anagram!"
       else
-        return false
+        return "No anagram for you"
       end
     else
       return "you need to input actual words"  
